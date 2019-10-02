2a3a99ab360358e7daeeb68c784fbc4771638873
bmlegge <36506641+bmlegge@users.noreply.github.com>
2019-10-02 12:47:36 -0400

    Added new consoleLine

--- DataMan/ConsoleUI/Program.cs
+++ DataMan/ConsoleUI/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+            Console.WriteLine("HelloAgain");
+            Console.ReadLine();
         }
     }
 }
