0c0c2ca391211d4028741c53d6a992f9fee78631
colemank0259 <43657783+colemank0259@users.noreply.github.com>
2019-10-07 11:57:00 -0400

    Created MemoryBank.cs

---
+++ DataMan/ConsoleUI/MemoryBank.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class MemoryBank
+    {
+        public static void getMemoryBank()
+        {
+            // Named constants
+            const int QUESTION_AMOUNT = 10;
+
+            // Local variables
+            Random rand = new Random();
+            int num1 = rand.Next(1, 100);
+            int num2 = 0;
+            char[] operators = { '+', '-', '*', '/' };
+            char randomOp = operators[rand.Next(operators.Length)];
+            
+        }
+        
+
+    }
+}
