1e08cab00e4a9ae829bd60942e51459c40a3a407
colemank0259 <43657783+colemank0259@users.noreply.github.com>
2019-10-09 12:52:24 -0400

    Updated MemoryBank.cs

--- DataMan/ConsoleUI/MemoryBank.cs
+++ DataMan/ConsoleUI/MemoryBank.cs
@@ -22,46 +22,89 @@ namespace ConsoleUI
             char randomOp = ' ';
             int userAnswer = 0;
             int correctAnswer = 0;
-            
-            // Loop for creating math problems
-            for (int i = 0; i <= QUESTION_AMOUNT; i++)
+            int runAnswer = 0;
+            bool run = true;
+
+            // Display description of the Memory Bank activity
+            Console.WriteLine("Enter the correct answer to 10 random math problems.");
+
+            do
             {
-                num1 = rand.Next(1, 100);
-                num2 = rand.Next(1, 100);
-                randomOp = operators[rand.Next(operators.Length)];
-                Console.Write($"{num1} {randomOp} {num2} = ");
-                inputString = Console.ReadLine();
-
-                if (int.TryParse(inputString, out userAnswer))
+                // Loop for creating math problems
+                for (int i = 0; i <= QUESTION_AMOUNT; i++)
                 {
-                    switch (randomOp)
+                    num1 = rand.Next(1, 100);
+                    num2 = rand.Next(1, 100);
+                    randomOp = operators[rand.Next(operators.Length)];
+                    Console.Write($"{num1} {randomOp} {num2} = ");
+                    inputString = Console.ReadLine();
+
+                    if (int.TryParse(inputString, out userAnswer))
                     {
-                        case '+':
-                            correctAnswer = num1 + num2;
-                            break;
-                        case '-':
-                            correctAnswer = num1 - num2;
-                            break;
-                        case '*':
-                            correctAnswer = num1 * num2;
-                            break;
-                        case '/':
-                            correctAnswer = num1 / num2;
-                            break;
+                        switch (randomOp)
+                        {
+                            case '+':
+                                correctAnswer = num1 + num2;
+                                break;
+                            case '-':
+                                correctAnswer = num1 - num2;
+                                break;
+                            case '*':
+                                correctAnswer = num1 * num2;
+                                break;
+                            case '/':
+                                correctAnswer = num1 / num2;
+                                break;
+                        }
+
+                        if (userAnswer == correctAnswer)
+                        {
+                            Console.WriteLine("Correct!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Incorrect.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not a valid response. Must be an integer.");
                     }
 
-                    if (userAnswer == correctAnswer)
+                    // Prompt the user to repeat the activity
+                    Console.Write("Enter 1 to repeat the activity or 2 to exit: ");
+                    inputString = Console.ReadLine();
+                    if (int.TryParse(inputString, out runAnswer))
                     {
-                        //TODO
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Not a valid response. Must be greater than 0."); 
-                }
+                        switch (runAnswer)
+                        {
+                            case 1:
+                                run = true;
+                                break;
+                            case 2:
+                                run = false;
+                                break;
+                           
+                        }
 
-               
-            }
+                        if (userAnswer == correctAnswer)
+                        {
+                            Console.WriteLine("Correct!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Incorrect.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not a valid response. Must be an integer.");
+                    }
+
+                }
+            } while (run == true);
+
+            
         }
         
 
