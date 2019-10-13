335dabb8d32639656fdd267a8962f9033da14af7
Jochs1989 <43485024+Jochs1989@users.noreply.github.com>
2019-10-13 11:46:11 -0400

    looking at memory bank

--- DataMan/ConsoleUI/MemoryBank.cs
+++ DataMan/ConsoleUI/MemoryBank.cs
@@ -8,7 +8,7 @@ namespace ConsoleUI
 {
     public class MemoryBank
     {
-        public static void GetMemoryBank()
+        public static void getMemoryBank()
         {
             // Named constants
             const int QUESTION_AMOUNT = 10;
@@ -20,47 +20,86 @@ namespace ConsoleUI
             int num2 = 0;
             char[] operators = { '+', '-', '*', '/' };
             char randomOp = ' ';
-            int userAnswer = 0;
-            int correctAnswer = 0;
-            
-            // Loop for creating math problems
-            for (int i = 0; i <= QUESTION_AMOUNT; i++)
-            {
-                num1 = rand.Next(1, 100);
-                num2 = rand.Next(1, 100);
-                randomOp = operators[rand.Next(operators.Length)];
-                Console.Write($"{num1} {randomOp} {num2} = ");
-                inputString = Console.ReadLine();
+            double userAnswer = 0;
+            double correctAnswer = 0;
+            int runAnswer = 0;
+            bool run = true;
 
-                if (int.TryParse(inputString, out userAnswer))
+            // Display description of the Memory Bank activity
+            Console.WriteLine("Enter the correct answer to 10 random math problems.");
+
+            do
+            {
+                // Loop for creating math problems
+                for (int i = 0; i <= QUESTION_AMOUNT; i++)
                 {
-                    switch (randomOp)
+                    num1 = rand.Next(1, 10);
+                    num2 = rand.Next(1, 10);
+                    randomOp = operators[rand.Next(operators.Length)];
+                    Console.Write($"{num1} {randomOp} {num2} = ");
+                    inputString = Console.ReadLine();
+
+                    if (double.TryParse(inputString, out userAnswer))
                     {
-                        case '+':
-                            correctAnswer = num1 + num2;
-                            break;
-                        case '-':
-                            correctAnswer = num1 - num2;
-                            break;
-                        case '*':
-                            correctAnswer = num1 * num2;
-                            break;
-                        case '/':
-                            correctAnswer = num1 / num2;
-                            break;
+                        switch (randomOp)
+                        {
+                            case '+':
+                                correctAnswer = num1 + num2;
+                                break;
+                            case '-':
+                                correctAnswer = num1 - num2;
+                                break;
+                            case '*':
+                                correctAnswer = num1 * num2;
+                                break;
+                            case '/':
+                                correctAnswer = num1 / num2;
+                                break;
+                        }
+
+                        if (userAnswer == correctAnswer)
+                        {
+                            Console.WriteLine("Correct!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Incorrect.");
+                        }
+
+                        // Consume next line for appearance
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not a valid response. Must be an integer.");
                     }
 
-                    if (userAnswer == correctAnswer)
+                    // TODO Find a way to format correct answers to division problems to only two decimal places
+
+                }
+
+                // Prompt the user to repeat the activity
+                Console.Write("Enter 1 to repeat the activity or 2 to exit: ");
+                inputString = Console.ReadLine();
+                if (int.TryParse(inputString, out runAnswer))
+                {
+                    switch (runAnswer)
                     {
-                        //TODO
+                        case 1:
+                            run = true;
+                            break;
+                        case 2:
+                            run = false;
+                            break;
+
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Not a valid response. Must be greater than 0."); 
+                    Console.WriteLine("Not a valid response. Must be an integer.");
                 }
-            }
-            Console.WriteLine();
+            } while (run == true);
+
         }
     }
 }
